a37408ff371e5267e6f7fa326ac64d33c59dd57b
amin <seyedamin963@gmail.com>
2022-01-29 13:29:07 +0330

    bug fixes

--- insurance/urls.py
+++ insurance/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import DiscountView, ListDiscountForSuperUser
 
 urlpatterns = [
-    # path('v1/insurance/', InsuranceView.as_view(), name='insurance_list'),
     path('v1/discount/', DiscountView.as_view(), name='discount_list'),
     path('v1/discount/superuser/', ListDiscountForSuperUser.as_view(), name='discount_superuser_list'),
 
--- insurance/views.py
+++ insurance/views.py
@@ -1,6 +1,7 @@
 from random import choices, choice
 from string import ascii_letters
 from django.db.models import Sum
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -23,7 +24,7 @@ def generate_body_discount():
     tmp = choices(third_discounts, weights=(15, 45, 40), k=1).pop()
     return tmp
 
-def discount_code():
+def generate_discount_code():
     letters = ascii_letters
     random_code = ''.join(choice(letters) for i in range(7))
     return random_code
@@ -35,6 +36,9 @@ class DiscountView(APIView):
     serializer_class_get = DiscountSerializerForGet
 
     def get(self, request, format=None):
+        """
+            get all of user discounts
+        """
         check_exist = Discount.objects.filter(user=request.user)
         if check_exist.exists():
             test = self.serializer_class_get(check_exist, many=True)
@@ -43,31 +47,42 @@ class DiscountView(APIView):
         else:
             return Response({'message':'تخفیفی برای شما ایجاد نشده است'})
         
-        return Response({'method':'GET'})
 
     def post(self, request):
+        """
+            post type of insurance and check if user has insurance
+             and return same else create a discount for user
+        """
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
+            # check insurance type the user wants to
+            # get a discount exist or not if insurance does not exist
+            # return 404 error
             insurance_type = serializer.validated_data.get('type')
-            ### exception
-            insurance_test = Insurance.objects.get(type=insurance_type)
+            insurance_test = get_object_or_404(Insurance, type=insurance_type)
+
+            
             discount_amount = 0
             if insurance_type == 'Third':
                 discount_amount = generate_third_discount()
 
-            else:
+            elif insurance_type == 'Body':
                 discount_amount = generate_body_discount()
+
+            else:
+                print('you should create Insurance object first')
+
 
             check_exist = Discount.objects.filter(type=insurance_test, user=request.user)
             if check_exist.exists():
-                test = self.serializer_class_get(check_exist, many=True)
+                d = self.serializer_class_get(check_exist, many=True)
                 response_message = {
                     'message':'شما قبلا یک تخفیف برای این نوع بیمه گرفته اید',
-                    'discount': test.data,
+                    'discount': d.data,
                 }
                 return Response(response_message)
-            generated_code = discount_code()
             
+            generated_code = generate_discount_code()
             discount_test = Discount.objects.create(
                 user = request.user,
                 type = insurance_test,
@@ -75,8 +90,8 @@ class DiscountView(APIView):
                 code = generated_code
             )
 
-            test = self.serializer_class_get(discount_test)
-            return Response({'discount': test.data}, status=status.HTTP_201_CREATED)
+            d = self.serializer_class_get(discount_test)
+            return Response({'discount': d.data}, status=status.HTTP_201_CREATED)
         else:
             return Response(
                 serializer.errors, 
@@ -88,19 +103,31 @@ class ListDiscountForSuperUser(APIView):
 
     serializer_class = DiscountSuperUserSerializer
     serializer_class_get = DiscountSerializerForGet
+
     permission_classes = (IsSuperUser,)
 
     def get(self, request, format=None):
-        return Response({'method':'get'})
+        """
+            get all discount and return for superuser
+        """
+        objects = Discount.objects.all()
+        serialized_discounts = self.serializer_class_get(objects, many=True)
+        return Response({'discounts': serialized_discounts.data})
         
 
     def post(self, request):
+        """
+            post type of discount and datetime then return 
+            all of discounts in posted hour and total amount 
+            of discounts for one hour 
+        """
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
             date = serializer.validated_data.get('date')
             insurance_type = serializer.validated_data.get('type')
-            insurance_test = Insurance.objects.get(type=insurance_type)
+            insurance_test = get_object_or_404(Insurance, type=insurance_type)
+
 
             objects = Discount.objects.filter(created_date__hour = date.hour, type=insurance_test)
             total = objects.aggregate(Sum('amount'))
@@ -108,7 +135,6 @@ class ListDiscountForSuperUser(APIView):
             return Response({
                 'total': total,
                 'discounts': serialize_object.data,
-                
             })
         
         else:
